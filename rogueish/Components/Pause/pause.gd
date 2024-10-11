860db4485e76ed5ca1095615ff69557cbe9a07a8
Sean Campbell <campbellsean211@gmail.com>
2024-10-11 11:53:56 -0400

    pause resumes, quits

--- rogueish/Components/Pause/pause.gd
+++ rogueish/Components/Pause/pause.gd
@@ -5,10 +5,29 @@ extends Control
 var offset = Vector2(0, -300)
 
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_menu"):
-		global_position = player.global_position + offset
-		visible = true
-		get_tree().paused = true
+	if Input.is_action_just_pressed("ui_menu"):
+		if (visible):
+			unpause()
+		else:
+			pause()
 
 func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	unpause()
+
+#CUSTOMS
+func pause() -> void:
+	global_position = player.global_position + offset
+	visible = true
+	get_tree().paused = true
+
+func unpause() -> void:
 	visible = false
+	get_tree().paused = false
+
+#RECIEVERS
+func _on_resume_pressed() -> void:
+	unpause()
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
